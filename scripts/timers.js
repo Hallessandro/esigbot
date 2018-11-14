a5aa6ecaffa1a6e724d1a766d07c0a1d059b8605
Hallessandro Dvilla <hd.jesus93@gmail.com>
2018-11-13 23:04:44 -0300

    Add arquivos .env para permitir conexao com o RocketChat

---
+++ scripts/timers.js
@@ -0,0 +1,15 @@
+module.exports = (robot) => {
+    // Description:
+    //    Retorna informações sobre os timers
+    // 
+    // Commands:
+    //    hubot qual a situação dos timers da (.*)? - responde a situação
+    //
+    robot.respond(/qual a situação dos timers da (.*)/, res => {
+        res.reply(res.match[1]);
+    });
+
+    robot.respond(/olá bot/, res => {
+        res.reply("Olá humano!");
+    });
+}
